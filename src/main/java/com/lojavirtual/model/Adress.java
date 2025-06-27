aae52ec5979429f599e815395ea4fc556402418c
Erick Saraiva <saraivaerick8@gmail.com>
2025-06-27 19:04:48 -0300

    Master - Removing unused import on Acess

--- src/main/java/com/lojavirtual/model/Adress.java
+++ src/main/java/com/lojavirtual/model/Adress.java
@@ -4,7 +4,6 @@
  */
 package com.lojavirtual.model;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.ConstraintMode;
 import jakarta.persistence.Entity;
 import jakarta.persistence.ForeignKey;
